f4bb2c544b7aa088eb20107fc72d761ad9346fdc
caesar <caesar@bitconch.io>
2019-12-15 19:17:04 +0800

    test_bank_parent_duplicate_signature to test_treasury_parent_duplicate_signature

--- runtime/src/treasury.rs
+++ runtime/src/treasury.rs
@@ -1498,7 +1498,7 @@ mod tests {
 
     /// Verifies that last ids and status cache are correctly referenced from parent
     #[test]
-    fn test_bank_parent_duplicate_signature() {
+    fn test_treasury_parent_duplicate_signature() {
         let (genesis_block, mint_keypair) = create_genesis_block(2);
         let key1 = Keypair::new();
         let parent = Arc::new(Bank::new(&genesis_block));
