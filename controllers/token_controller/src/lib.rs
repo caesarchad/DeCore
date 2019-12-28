214ccbc73a752484eac38cdb0e6d600582cef48c
caesarchad <blue.web.geek@gmail.com>
2019-12-28 15:19:56 +0800

    change token api crate name to ico_issuer

--- controllers/token_controller/src/lib.rs
+++ controllers/token_controller/src/lib.rs
@@ -1,10 +1,10 @@
 #[macro_export]
 macro_rules! morgan_token_controller {
     () => {
-        ("morgan_token_controller".to_string(), morgan_token_api::pgm_id::id())
+        ("morgan_token_controller".to_string(), morgan_ico_issuer::pgm_id::id())
     };
 }
 
-use morgan_token_api::ico_handler::handle_opcode;
+use morgan_ico_issuer::ico_handler::handle_opcode;
 
 morgan_interface::morgan_entrypoint!(handle_opcode);
