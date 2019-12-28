094e21006020bdb2726925fa18e91268fff3d842
caesarchad <blue.web.geek@gmail.com>
2019-12-28 11:11:34 +0800

    merge config_controller with profiler

--- controllers/config_controller/src/lib.rs
+++
@@ -1,9 +0,0 @@
-#[macro_export]
-macro_rules! morgan_config_controller {
-    () => {
-        ("morgan_config_controller".to_string(), morgan_profiler::pgm_id::id())
-    };
-}
-use morgan_profiler::handler::handle_opcode;
-
-morgan_interface::morgan_entrypoint!(handle_opcode);
